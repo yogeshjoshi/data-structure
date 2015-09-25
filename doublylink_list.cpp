31cddb5b47527f68f6872d1939e8e2efeeab48ef
yogeshjoshi <joshiyogesh001@gmail.com>
2015-09-25 18:19:10 +0530

    awayed compiltion error still have problem in logic

--- doublylink_list.cpp
+++ doublylink_list.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+ #include<iostream>
 #include<malloc.h>
 using namespace std;
 class doublylink_list{
@@ -10,7 +10,7 @@ int main(){
     int no_of_node,node_count=1,location;
     cout<<"enter the number of element to form link list"<<endl;
     cin>>no_of_node;
-class doublylink_list *start_node,*current_node,*temporary_node;
+class doublylink_list *start_node,*current_node,*temporary_node,*restore_node;
 while(node_count<=no_of_node){
 current_node=(class doublylink_list*)malloc(sizeof(class doublylink_list));
 cout<<"enter the numbers";
@@ -22,7 +22,7 @@ if(node_count==1){
     temporary_node=current_node;
 }
 else{
-temporary_node->previous_link=temporary_node;
+current_node->previous_link=temporary_node;
 temporary_node->link=current_node;
 temporary_node=current_node;
 }
@@ -34,27 +34,46 @@ cout<<"info written in node are ,,,,,,"<<endl;
 temporary_node=start_node;
 				for(int i=1;i<=no_of_node;i++)
 				{
+				    restore_node=temporary_node;
 					cout<<temporary_node->info<<endl;
 					if (i==1){
-					temporary_node=temporary_node->link;
-				}
+					temporary_node=temporary_node->link;}
 				else{
-                    temporary_node->previous_link=temporary_node;
-                    temporary_node=temporary_node->link;
+                    temporary_node->previous_link=restore_node;
+                    temporary_node=temporary_node->link;}
 				}
-				}
-cout<<"hello";
+//cout<<"hello";
 //to check either doubly link list is working or not...
-cout<<"enter the location to find its previous element";
+cout<<"enter the location to find its previous element"<<endl;
 cin>>location;
-for(int i=0;i<=no_of_node;i++){
+temporary_node=start_node;
+for(int i=1;i<=no_of_node;i++){
+if(location==1){
+    cout<<"no element exist in 0 th position."<<endl;
+    temporary_node=temporary_node->link;
+    break;
+}
+else{
     if(i==location){
-        temporary_node=temporary_node->previous_link;
-        cout<<"answer is..."<<temporary_node->info;
+
+        cout<<"answer is..."<<restore_node->info<<endl;
     }
+    restore_node=temporary_node;
+ temporary_node->previous_link=restore_node;
     temporary_node=temporary_node->link;
 
 }
+}
+// no of node in link list...
+int count=1;
+temporary_node=start_node;
+while(temporary_node->link != NULL){
+    count++;
+    temporary_node=temporary_node->link;
+}
+cout<<"no of node in link list is equal to ..."<<count<<endl;
+
+
 
 
 }
